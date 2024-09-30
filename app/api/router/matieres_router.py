4e617528dcc41aa6fb2dcbcf47485ed1b919a8ee
GaelBuenoBarthe <gaelbueno@aol.com>
2024-09-30 16:50:08 +0200

    [fix] all call on swagger

--- app/api/router/matieres_router.py
+++ app/api/router/matieres_router.py
@@ -1,14 +1,18 @@
 from fastapi import APIRouter, Depends, HTTPException
 from typing import List
+
+from pymongo.synchronous.database import Database
+
 from app.domain.schemas.matieres_schema import MatiereSchema
 from app.api.controller.matieres_controller import get_all_matieres, get_matiere_by_id
+from app.util.mongo_singleton import MongoSingleton
 
 router = APIRouter()
 
 @router.get("/", response_model=List[MatiereSchema])
-async def read_matieres():
-    return await get_all_matieres()
+async def read_matieres(db: Database = Depends(MongoSingleton.get_db)):
+    return await get_all_matieres(db)
 
 @router.get("/{idmatiere}", response_model=MatiereSchema)
-async def read_matiere(idmatiere: int):
-    return await get_matiere_by_id(idmatiere)
+async def read_matiere(idmatiere: int, db: Database = Depends(MongoSingleton.get_db)):
+    return await get_matiere_by_id(idmatiere, db)
