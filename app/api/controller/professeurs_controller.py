8eca4cc81fc77935e65c5d2080c743206ddb157b
GaelBuenoBarthe <gaelbueno@aol.com>
2024-10-01 11:59:23 +0200

    .

--- app/api/controller/professeurs_controller.py
+++ app/api/controller/professeurs_controller.py
@@ -1,7 +1,10 @@
-from fastapi import HTTPException
+from http.client import HTTPException
+from fastapi import APIRouter, Depends
 from pymongo.database import Database
 from app.util.mongo_singleton import MongoSingleton
-from app.domain.schemas.professeurs_schema import ProfesseurCreateSchema, ProfesseurUpdateSchema
+from app.domain.schemas.professeurs_response_schema import ProfesseurResponseSchema
+from app.domain.schemas.professeur_update_schema import ProfesseurUpdateSchema
+from app.domain.schemas.professeur_create_schema import ProfesseurCreateSchema
 
 
 # Fetch all professors
@@ -14,32 +17,39 @@ async def get_all_professeurs(db: Database):
 async def get_professeur_by_id(professeur_id: int, db: Database):
     professeur = db.professeurs.find_one({"id": professeur_id}, projection={"_id": False})
     if not professeur:
-        raise HTTPException(status_code=404, detail="Professeur not found")
+        raise HTTPException(status_code=404, detail="Professeur non trouvé")
     return professeur
 
 
-# Create new professor
-async def create_professeur(professeur: ProfesseurCreateSchema, db: Database):
+# Creation d'un nouveau professeur
+async def create_professeur(professeur: ProfesseurCreateSchema, db: Database) -> ProfesseurResponseSchema:
     existing_professeur = db.professeurs.find_one({"id": professeur.id})
 
     if existing_professeur:
-        raise HTTPException(status_code=400, detail="Professeur with this ID already exists")
+        raise HTTPException(status_code=400, detail="Professeur avec cette ID existe déjà")
 
-    professeur_dict = professeur.dict()
+    professeur_dict = professeur.model_dump()
     db.professeurs.insert_one(professeur_dict)
-    return {"message": "Professeur created successfully", "professeur_id": professeur.id}
+
+    # Retourne le professeur créé
+    return ProfesseurResponseSchema(**professeur_dict)
 
 
 # Update professor by ID
-async def update_professeur(professeur_id: int, professeur: ProfesseurUpdateSchema, db: Database):
+async def update_professeur(professeur_id: int, professeur: ProfesseurUpdateSchema, db: Database) -> ProfesseurResponseSchema:
     existing_professeur = db.professeurs.find_one({"id": professeur_id})
 
     if not existing_professeur:
-        raise HTTPException(status_code=404, detail="Professeur not found")
+        raise HTTPException(status_code=404, detail="Professeur non trouvé")
 
-    updated_data = {k: v for k, v in professeur.dict().items() if v is not None}  # Update only non-null fields
+    updated_data = {k: v for k, v in professeur.model_dump().items() if v is not None}
     db.professeurs.update_one({"id": professeur_id}, {"$set": updated_data})
-    return {"message": "Professeur updated successfully"}
+
+    # Fetch the updated professor data
+    updated_professeur = db.professeurs.find_one({"id": professeur_id}, projection={"_id": False})
+
+    # Return the updated professor as a response
+    return ProfesseurResponseSchema(**updated_professeur)
 
 
 # Delete professor by ID
@@ -47,6 +57,6 @@ async def delete_professeur(professeur_id: int, db: Database):
     result = db.professeurs.delete_one({"id": professeur_id})
 
     if result.deleted_count == 0:
-        raise HTTPException(status_code=404, detail="Professeur not found")
+        raise HTTPException(status_code=404, detail="Professeur non trouvé")
 
-    return {"message": "Professeur deleted successfully"}
+    return {"message": "Professeur supprimer avec succés"}
--- app/api/router/professeurs_router.py
+++ app/api/router/professeurs_router.py
@@ -3,11 +3,12 @@ from typing import List
 from fastapi import APIRouter, Depends
 from pymongo.database import Database
 
-
-from app.api.controller.professeurs_controller import *
-
-from app.domain.schemas.professeurs_schema import ProfesseurCreateSchema, ProfesseurUpdateSchema
+from app.api.controller.professeurs_controller import get_all_professeurs, get_professeur_by_id, create_professeur, \
+    update_professeur, delete_professeur
 from app.util.mongo_singleton import MongoSingleton
+from app.domain.schemas.professeurs_response_schema import ProfesseurResponseSchema
+from app.domain.schemas.professeur_update_schema import ProfesseurUpdateSchema
+from app.domain.schemas.professeur_create_schema import ProfesseurCreateSchema
 
 router = APIRouter()
 
@@ -19,12 +20,14 @@ async def read_professeurs(db: Database = Depends(MongoSingleton.get_db)):
 async def read_professeur(professeur_id: int, db: Database = Depends(MongoSingleton.get_db)):
     return await get_professeur_by_id(professeur_id, db)
 
-@router.post("/", response_model=ProfesseurCreateSchema)
+@router.post("/", response_model=ProfesseurResponseSchema)
 async def create_professeur_endpoint(professeur: ProfesseurCreateSchema, db: Database = Depends(MongoSingleton.get_db)):
+    from app.api.controller.professeurs_controller import create_professeur
     return await create_professeur(professeur, db)
 
-@router.put("/{professeur_id}", response_model=ProfesseurCreateSchema)
+@router.put("/{professeur_id}", response_model=ProfesseurResponseSchema)
 async def update_professeur_endpoint(professeur_id: int, professeur: ProfesseurUpdateSchema, db: Database = Depends(MongoSingleton.get_db)):
+    from app.api.controller.professeurs_controller import update_professeur
     return await update_professeur(professeur_id, professeur, db)
 
 @router.delete("/{professeur_id}")
